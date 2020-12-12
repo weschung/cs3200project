c90a23f530031deacb8440872643d7d74596ad15
Wesley Chung <chung.wes@northeastern.edu>
2020-12-11 19:13:33 -0500

    user editor

--- src/main/webapp/users/user-editor.js
+++ src/main/webapp/users/user-editor.js
@@ -13,44 +13,101 @@ class UserEditor extends React.Component {
         this.findUserById(id)
     }
 
-    saveUser = () =>
+    saveUsername = () =>
         renameUser(this.state.user)
 
-    // saveUser = () =>
-    //     changePassword(this.state.user)
-    //
-    // saveUser = () =>
-    //     changeDob(this.state.user)
-    //
-    // saveUser = () =>
-    //     changeFirst(this.state.user)
-    //
-    // saveUser = () =>
-    //     changeLast(this.state.user)
-    //
-    // saveUser = () =>
-    //     changeEmail(this.state.user)
+    savePassword = () =>
+        changePassword(this.state.user)
+
+    saveDob = () =>
+        changeDob(this.state.user)
+
+    saveFirst = () =>
+        changeFirst(this.state.user)
+
+    saveLast = () =>
+        changeLast(this.state.user)
+
+    saveEmail = () =>
+        changeEmail(this.state.user)
 
     render() {
         return(
             <div className="container">
                 <h1>User Editor: <br/> {this.state.user.username}</h1>
-                <input className="form-control" readOnly={true} value={this.state.user.id}/>
-                <input
+
+                ID: <input className="form-control" readOnly={true}
+                       value={this.state.user.id}/> <br/>
+
+                Username: <input
                     onChange={(event) => this.setState({
                         user: {
                             ...this.state.user,
-                            username: event.target.value
-                        }
-                    })}
+                        username: event.target.value}})}
                     className="form-control"
                     value={this.state.user.username}/>
-                <button onClick={this.saveUser}>
-                    Save
-                </button>
+                <button onClick={this.saveUsername}>
+                    Save Username
+                </button> <br/> <br/>
+
+                Password: <input
+                    onChange={(event) => this.setState({
+                        user: {
+                            ...this.state.user,
+                            password: event.target.value}})}
+                    className="form-control"
+                    value={this.state.user.password}/>
+                <button onClick={this.savePassword}>
+                    Save Password
+                </button> <br/> <br/>
+
+                First Name: <input
+                    onChange={(event) => this.setState({
+                        user: {
+                            ...this.state.user,
+                            first_name: event.target.value}})}
+                    className="form-control"
+                    value={this.state.user.first_name}/>
+                <button onClick={this.saveFirst}>
+                    Save First Name
+                </button> <br/> <br/>
+
+                Last Name: <input
+                    onChange={(event) => this.setState({
+                        user: {
+                            ...this.state.user,
+                            last_name: event.target.value}})}
+                    className="form-control"
+                    value={this.state.user.last_name}/>
+                <button onClick={this.saveLast}>
+                    Save Last Name
+                </button> <br/> <br/>
+
+                Date of Birth (format YYYY-MM-DD 00:00:00): <input
+                    onChange={(event) => this.setState({
+                        user: {
+                            ...this.state.user,
+                            dob: event.target.value}})}
+                    className="form-control"
+                    value={this.state.user.dob}/>
+                <button onClick={this.saveDob}>
+                    Save Date of Birth
+                </button> <br/> <br/>
+
+                Email: <input
+                    onChange={(event) => this.setState({
+                        user: {
+                            ...this.state.user,
+                            email: event.target.value}})}
+                    className="form-control"
+                    value={this.state.user.email}/>
+                <button onClick={this.saveEmail}>
+                    Save Email
+                </button> <br/> <br/>
+
                 <a href="user-list.html">
                     Done
-                </a>
+                </a> <br/> <br/> <br/> <br/> <br/> <br/>
             </div>
         )
     }
