b4182ea585f6174879579c538631bf885f3c9256
fzyang1227 <62119227+fzyang1227@users.noreply.github.com>
2020-12-13 21:37:35 -0500

    address/phone dao find by user id

--- src/main/java/com/example/wap/daos/AddressDao.java
+++ src/main/java/com/example/wap/daos/AddressDao.java
@@ -1,16 +1,23 @@
 package com.example.wap.daos;
 
 import com.example.wap.models.Address;
+import com.example.wap.models.Phone;
 import com.example.wap.repositories.AddressRepository;
+import com.example.wap.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class AddressDao {
     @Autowired
     AddressRepository addressRepository;
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/findAllAddresses")
     public Iterable<Address> findAllAddresses() {
         return addressRepository.findAll();
@@ -19,6 +26,11 @@ public class AddressDao {
     public Address findAddressById(
             @PathVariable("id") Integer id) {
         return addressRepository.findById(id).get();
+    }
+    @GetMapping("/findAddressByUserId/{user_id}")
+    public List<Address> findAddressByUserId(
+            @PathVariable("user_id") Integer user_id) {
+        return userRepository.findById(user_id).get().getAddresses();
     }
     @GetMapping("/deleteAddress/{id}")
     public void deleteAddress(
--- src/main/java/com/example/wap/daos/PhoneDao.java
+++ src/main/java/com/example/wap/daos/PhoneDao.java
@@ -2,15 +2,21 @@ package com.example.wap.daos;
 
 import com.example.wap.models.Phone;
 import com.example.wap.repositories.PhoneRepository;
+import com.example.wap.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class PhoneDao {
     @Autowired
     PhoneRepository phoneRepository;
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/findAllPhones")
     public Iterable<Phone> findAllPhones() {
         return phoneRepository.findAll();
@@ -19,6 +25,11 @@ public class PhoneDao {
     public Phone findPhoneById(
             @PathVariable("id") Integer id) {
         return phoneRepository.findById(id).get();
+    }
+    @GetMapping("/findPhoneByUserId/{user_id}")
+    public List<Phone> findPhoneByUserId(
+            @PathVariable("user_id") Integer user_id) {
+        return userRepository.findById(user_id).get().getPhones();
     }
     @GetMapping("/deletePhone/{id}")
     public void deletePhone(
